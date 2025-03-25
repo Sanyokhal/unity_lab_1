72e2b42df6bed8fb09cac818618ccf52556c813c
Sanyokhal <halsasha724@gmail.com>
2025-03-25 08:48:23 +0200

    Lab-2

--- Assets/Scripts/UserMovement.cs
+++ Assets/Scripts/UserMovement.cs
@@ -5,8 +5,11 @@ public class UserMovement : MonoBehaviour
     public float player_speed = 10.0f;
     public float jump_height = 10.0f;
 
+    public float timeLeft = 0f;
+    public float maxSprintDuration = 3.0f;
+    public bool use_speed_boost = false;
     private bool onTheGround = true;
-
+    private bool touchedFinish = false;
     private float horizontal_input;
     private float vertical_input;
     private Rigidbody playerRB;
@@ -28,6 +31,23 @@ public class UserMovement : MonoBehaviour
             playerRB.AddForce(Vector3.up * jump_height, ForceMode.Impulse);
             onTheGround = false;
         }
+        if(Input.GetKeyDown(KeyCode.LeftShift) && timeLeft < maxSprintDuration){
+            if(!use_speed_boost){
+            Debug.Log("Speed boost");
+            use_speed_boost = true;
+            player_speed *= 2;
+            }
+            timeLeft += Time.deltaTime;
+        }else{
+            timeLeft -= Time.deltaTime;
+            if(timeLeft < 0){
+                timeLeft = 0f;
+                if(use_speed_boost){
+use_speed_boost = false;
+                player_speed /=2;
+                }
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -35,6 +55,10 @@ public class UserMovement : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             onTheGround = true;
+        }else if(collision.gameObject.CompareTag("Finish") && !touchedFinish){
+            Debug.Log("FINISH");
+            touchedFinish = true;
         }
+
     }
 }
